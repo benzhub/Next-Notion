abb2d0e8b07189b975cbcaa3d02c60865146b024
Ben <ben@kgs.tw>
2024-06-22 00:55:34 +0800

    feat: Add search functionality and recent articles section to SideBar component

--- app/_components/side-bar.tsx
+++ app/_components/side-bar.tsx
@@ -1,12 +1,24 @@
 function SideBar() {
   return (
-    <ul>
-      <li>itemsitemsitems</li>
-      <li>itemsitemsitems</li>
-      <li>itemsitemsitems</li>
-      <li>itemsitemsitems</li>
-      <li>itemsitemsitems</li>
-      <li>itemsitemsitems</li>
+    <ul className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="搜尋"
+        className="outline-none p-2 rounded"
+      />
+
+      <section className="flex flex-col gap-1">
+        <h4 className="font-bold text-xl text-gray-500">近期文章</h4>
+        <div className="bg-primary h-[2px] w-full" />
+        <ul className="flex flex-col gap-2 px-1 py-2">
+          <li>itemsitemsitems</li>
+          <li>itemsitemsitems</li>
+          <li>itemsitemsitems</li>
+          <li>itemsitemsitems</li>
+          <li>itemsitemsitems</li>
+        </ul>
+      </section>
+
       <li>itemsitemsitems</li>
       <li>itemsitemsitems</li>
       <li>itemsitemsitems</li>
