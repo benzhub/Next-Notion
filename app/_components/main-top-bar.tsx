8ddb09cfd474b8ce5637c2ab584378bd580eef41
Ben <ben@kgs.tw>
2024-06-22 00:40:56 +0800

    refactor: Add MainTopBar component and update Main Template layout

---
+++ app/_components/main-top-bar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { HiMenuAlt1 } from "react-icons/hi";
+
+function MainTopBar({ toggleFn }: { toggleFn: () => void }) {
+    const pathname = usePathname();
+  return (
+    <nav className="px-4 pt-4 flex justify-between items-center border-b-4 border-primary bg-neutral text-slate-300">
+      <div className="w-16 h-8 bg-gray-500 -translate-y-1"></div>
+      <div>
+        <ul className="hidden md:flex items-center justify-between px-4">
+          <li className={`py-2 px-4 ${pathname === '/blog/python' ? 'bg-primary' : ''}`}>
+            <Link href="/blog/python">Python</Link>
+          </li>
+          <li className={`py-2 px-4 ${pathname === '/blog/golang' ? 'bg-primary' : ''}`}>
+            <Link href="/blog/golang">Golang</Link>
+          </li>
+          <li className={`py-2 px-4 ${pathname === '/blog/javascript' ? 'bg-primary' : ''}`}>
+            <Link href="/blog/javascript">Javascript</Link>
+          </li>
+          <li className={`py-2 px-4 ${pathname === '/blog/java' ? 'bg-primary' : ''}`}>
+            <Link href="/blog/java">Java</Link>
+          </li>
+        </ul>
+        <HiMenuAlt1
+          size={32}
+          onClick={toggleFn}
+          className="cursor-pointer md:hidden"
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default MainTopBar;
--- app/_templates/main-template.tsx
+++ app/_templates/main-template.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { ReactNode, useState } from "react";
-import { FaBookOpen, FaUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
-import { HiMenuAlt1 } from "react-icons/hi";
+import { FaBookOpen, FaUser } from "react-icons/fa6";
 import { TiHome } from "react-icons/ti";
+import MainTopBar from "../_components/main-top-bar";
 import SideBar from "../_components/side-bar";
 
 function HomePageTemplate({ children }: { children: ReactNode }) {
@@ -13,18 +13,7 @@ function HomePageTemplate({ children }: { children: ReactNode }) {
   const toggleDropDown = () => setIsSidebarOpen(prev => !prev);
   return (
     <div className="h-[100dvh] grid grid-rows-[auto_1fr_auto]">
-      <nav className="p-4 flex justify-between items-center border-b-4 border-primary bg-neutral text-slate-300">
-        <div className="w-16 h-8 bg-gray-500"></div>
-        <div>
-          <ul className="hidden md:flex items-center justify-between gap-4 px-4">
-            <li>11111</li>
-            <li>22222</li>
-            <li>33333</li>
-            <li>44444</li>
-          </ul>
-          <HiMenuAlt1 size={32} onClick={toggleDropDown} className="cursor-pointer md:hidden"/>
-        </div>
-      </nav>
+      <MainTopBar toggleFn={toggleDropDown}/>
       <div className={`overflow-y-scroll ${isSidebarOpen ? 'relative' : ''} md:grid md:grid-cols-[1fr_auto]`}>
         <main className="overflow-y-scroll">{children}</main>
         <div className={`p-4 overflow-y-scroll bg-neutral-content transition-all duration-400 delay-50 ${isSidebarOpen ? '!bg-[#eaeaec] absolute md:static top-0 w-full md:w-auto' : 'w-0 h-0 invisible md:w-auto md:h-auto md:visible'}`}>
