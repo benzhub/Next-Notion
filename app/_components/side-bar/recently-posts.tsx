b0f8f57aebb93420dbdd8d7a476609bd3310c0c7
Ben <ben@kgs.tw>
2024-06-23 11:06:52 +0800

    feat: Update RecentlyPosts component to display only the first 5 blog posts

--- app/_components/side-bar/recently-posts.tsx
+++ app/_components/side-bar/recently-posts.tsx
@@ -11,7 +11,7 @@ function RecnetlyPosts() {
       <h4 className="font-bold text-xl text-gray-500">近期文章</h4>
       <div className="bg-primary h-[3px] w-full" />
       <ul className="flex flex-col gap-2 px-1 py-2">
-        {blogs.list.map((blog) => (
+        {blogs.list.slice(0, 5).map((blog) => (
           <li key={blog.id} className="hover:text-primary cursor-pointer">
             <Link href={`/blog/${blog.properties.Slug.rich_text[0].text.content}`}>{blog.properties.Name.title[0].plain_text}</Link>
           </li>
