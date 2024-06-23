c0e3ce7eaf15a97f8b8e465a7b8cec5d375e49d0
Ben <ben@kgs.tw>
2024-06-23 16:51:06 +0800

    refactor: Update SideBarTemplate to improve sidebar visibility and remove unnecessary code

--- app/_components/side-bar/template.tsx
+++ app/_components/side-bar/template.tsx
@@ -9,11 +9,11 @@ function SideBarTemplate({
 }) {
   return (
     <div
-      className={`p-4 h-full overflow-y-scroll bg-neutral-content transition-all duration-400 delay-50 ${
+      className={`p-4 overflow-y-scroll bg-neutral-content transition-all duration-400 delay-50 ${
         isOpen
           ? "!bg-[#eaeaec] absolute md:static top-0 w-full md:w-auto"
-          : "hidden"
-      } md:block md:w-auto md:h-auto`}
+          : "invisible h-0"
+      } md:visible md:w-auto md:h-auto`}
     >
       <div className="md:max-w-[240px] lg:max-w-[280px] h-[100dvh] mx-auto">
         <ul className="flex flex-col gap-4">{children}</ul>
